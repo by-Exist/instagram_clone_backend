0485c446fdc2c75fb116e6c8b698f9d7da3d146d
Exist <bolk9652@naver.com>
2020-11-22 20:05:07 +0900

    ✨feature: accounts 앱 추가 및 User 모델 정의

---
+++ accounts/models.py
@@ -0,0 +1,28 @@
+from typing import AbstractSet
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
+
+
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성"
+        FEMAIL = "W", "여성"
+
+    follower_set = models.ManyToManyField("self", blank=True)
+    following_set = models.ManyToManyField("self", blank=True)
+
+    website_url = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+    phone_number = models.CharField(
+        max_length=13,
+        blank=True,
+        validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")],
+    )
+    gender = models.CharField(max_length=1, blank=True, choices=GenderChoices.choices)
+
+    avatar = models.ImageField(
+        blank=True,
+        upload_to="accounts/avatar/%Y/%m/%d",
+        help_text="48px * 48px 크기의 png/jpg 파일을 업로드해주세요.",
+    )
