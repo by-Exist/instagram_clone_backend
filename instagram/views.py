d6c4ac078f17fe2ec879b162c5ee7334095a1578
Exist <bolk9652@naver.com>
2020-11-24 14:41:37 +0900

    ✨feature: PostList 전달 queryset 수정
    { Authorization: `JWT ${jwtToken}` } 요청이 헤드에 담기니
    permission_classes 없이 인증이 해결되었다는 점을 기억하자.

--- instagram/views.py
+++ instagram/views.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+from django.db.models import Q
 from django.shortcuts import render
+from django.utils import timezone
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 from .models import Post
@@ -8,4 +11,12 @@ from .serializers import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [AllowAny]  # FIXME: 인증 구현
+
+    def get_queryset(self):
+        time_since = timezone.now() - timedelta(days=3)
+        qs = super().get_queryset()
+        login_user = self.request.user
+        qs = qs.filter(
+            Q(author=login_user) | Q(author__in=login_user.following_set.all())
+        ).filter(created_at__gte=time_since)
+        return qs
