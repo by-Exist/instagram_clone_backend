224f53fbb7a9a52cadc6ea5ff41b044afa12889b
Exist <bolk9652@naver.com>
2020-11-24 15:36:58 +0900

    SuggestionList 제공 API 구현

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from typing import AbstractSet
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
+from django.shortcuts import resolve_url
 
 
 class User(AbstractUser):
@@ -26,3 +27,14 @@ class User(AbstractUser):
         upload_to="accounts/avatar/%Y/%m/%d",
         help_text="48px * 48px 크기의 png/jpg 파일을 업로드해주세요.",
     )
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}".strip()
+
+    @property
+    def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        else:
+            return resolve_url("pydenticon_image", self.username)
