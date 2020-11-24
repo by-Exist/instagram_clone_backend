cf24b6737d4451429bd5d8c9fb09c1dcbacdc780
Exist <bolk9652@naver.com>
2020-11-24 17:30:47 +0900

    🚅perfs: 쿼리 속도 향상 및 하는 방법 기록
    select_related()와 prefetch_related()를 활용하여 쿼리 속도를 증가시키는 방법을 알게 되었다.

--- instagram/views.py
+++ instagram/views.py
@@ -9,7 +9,15 @@ from .serializers import PostSerializer
 
 
 class PostViewSet(ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = (
+        # Post List API에서 SQL 요청을 보며 반복되는 WHERE에 집중한다.
+        # Post : 1로 표현, select_related => accounts_user에 해당
+        # Post : N으로 표현, prefetch_related => teg_set, like_user_set에 해당
+        Post.objects.all()
+        .select_related("author")
+        .prefetch_related("tag_set", "like_user_set")
+        # 쿼리 속도/갯수가 9.00ms/22개에서 5.00ms/9개로 줄어들었다.
+    )
     serializer_class = PostSerializer
 
     def get_queryset(self):
