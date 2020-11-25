2bd03d0fceadc57467314c907b19acfb920c7f95
Exist <bolk9652@naver.com>
2020-11-25 20:54:24 +0900

    🐞fix: User 모델 API의 avatar_url 형식 변경
    avatar_url 반환 형식에 도메인이 자동으로 추가되도록 변경

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -2,9 +2,22 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.fields import ReadOnlyField
 from .models import Post, Comment
+import re
 
 
 class AuthorSerializer(serializers.ModelSerializer):
+
+    avatar_url = serializers.SerializerMethodField("avatar_url_field")
+
+    def avatar_url_field(self, author):
+        if re.match(r"^https?://", author.avatar_url):
+            return author.avatar_url
+
+        if "request" in self.context:
+            scheme = self.context["request"].scheme
+            host = self.context["request"].get_host()
+            return scheme + "://" + host + author.avatar_url
+
     class Meta:
         model = get_user_model()
         fields = ["username", "name", "avatar_url"]
--- instagram/views.py
+++ instagram/views.py
@@ -59,6 +59,12 @@ class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
+    # serializer에서 context를 활용할 수 있게 이런 방식을 지원할 수 있다.
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
+
     def get_queryset(self):
         qs = super().get_queryset()
         qs = qs.filter(post__pk=self.kwargs["post_pk"])
@@ -68,3 +74,4 @@ class CommentViewSet(ModelViewSet):
         post = get_object_or_404(Post, pk=self.kwargs["post_pk"])
         serializer.save(author=self.request.user, post=post)
         return super().perform_create(serializer)
+
